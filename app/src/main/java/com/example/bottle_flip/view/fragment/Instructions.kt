26c68def08629665ac4f944c928751976abe1618
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2024-10-27 12:45:07 -0500

    add instructions frame content

--- app/src/main/java/com/example/bottle_flip/view/fragment/Instructions.kt
+++ app/src/main/java/com/example/bottle_flip/view/fragment/Instructions.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.bottle_flip.R
 
 import com.example.bottle_flip.databinding.InstructionsBinding
 
@@ -22,5 +25,23 @@ class Instructions : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Load gif winner using Glide
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.win_animated)
+            .into(binding.ivAnimatedWin)
+
+        // Configuration of back button
+        binding.btnBack.setOnClickListener {
+            findNavController().navigate(R.id.action_instructions_to_game)
+        }
+
+        // Call to pause audio game function
+        checkAndPauseBackgroundAudio()
+    }
+
+    // Declaration of pause audio game function
+    private fun checkAndPauseBackgroundAudio() {
     }
 }
