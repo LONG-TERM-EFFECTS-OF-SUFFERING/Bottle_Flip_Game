9a3e74f3d5e88e284bcc7bede2c1b74d292cbf57
trijuank <juan.lopez.quitana@correounivalle.edu.co>
2024-10-30 20:19:55 -0500

    Regenerar funcion de botella y agregar funciones al toolbar

--- app/src/main/java/com/example/bottle_flip/view/fragment/Game.kt
+++ app/src/main/java/com/example/bottle_flip/view/fragment/Game.kt
@@ -11,160 +11,270 @@ import android.os.CountDownTimer
 import com.example.bottle_flip.R
 import android.widget.ImageView
 import com.airbnb.lottie.LottieAnimationView
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ValueAnimator
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
+import androidx.core.view.isVisible
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.bottle_flip.databinding.GameBinding
 
 class Game : Fragment() {
 
-    private lateinit var countdownText: TextView
+    private lateinit var countdownText: TextView    //texto contador
+    private lateinit var countdownTimer: CountDownTimer //Tiempo contador
     private var backgroundMediaPlayer: MediaPlayer? = null
     private var effectMediaPlayer: MediaPlayer? = null // Para el sonido de efecto
     private lateinit var bottleImage: ImageView
     private lateinit var btnSpin: LottieAnimationView
     private var lastAngle = 0f // Variable para almacenar el último ángulo de la botella
 
+    private lateinit var navController: NavController
+    private lateinit var audioBackground: MediaPlayer
+    private lateinit var audioSpinBottle: MediaPlayer
+    private lateinit var audioShowChallenge: MediaPlayer
+    private lateinit var audioButton: MediaPlayer
+    private lateinit var audioSuspense: MediaPlayer
+    private lateinit var binding: GameBinding  //Acceder a los componenetes de la vista principal
+    private var isMute: Boolean = true
+
+
+    private val _statusShowDialog = MutableLiveData(false)
+    val statusShowDialog: LiveData<Boolean> get() = _statusShowDialog
+
+    private val _statusRotationBottle = MutableLiveData(false)
+    val statusRotationBottle: LiveData<Boolean> get() = _statusRotationBottle
+
+    private val _rotationBotle = MutableLiveData<RotateAnimation>()
+    val rotationBottle: LiveData<RotateAnimation> get() = _rotationBotle
+
+    private val _enableButton = MutableLiveData(true)
+    val enableButton: LiveData<Boolean> get() = _enableButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.game, container, false)
-        countdownText = view.findViewById(R.id.countdownText)
+        binding = GameBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
 
-        // Inicializar el ImageView de la botella y el botón LottieAnimationView
-        bottleImage = view.findViewById(R.id.centerImageView)
-        btnSpin = view.findViewById(R.id.btn_spin)
-        countdownText = view.findViewById(R.id.countdownText)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        menu(view) //Manejar el menu
+        media()    //Manejar el sonido
+        bottle()   //Manejar la botella
+    }
 
-        // Reproducir el sonido de fondo al iniciar el fragmento
-        playBackgroundSound()
-
-        // Configurar el OnClickListener para que al presionar el botón gire la botella
-        btnSpin.setOnClickListener {
-            // Iniciar la animación de Lottie (opcional si quieres que el botón también se anime)
-            btnSpin.playAnimation()
-            // Iniciar la animación de rotación de la botella
-            playBottleSound() // Reproduce el sonido de la botella solo al presionar el botón
-            stopBackgroundSound() // Detiene el sonido de fondo
-            playEffectSound() // Reproduce el sonido de efecto inicia el giro de la botella
-            rotateBottleClockwise()
+    //XXXXXXXXXXXXXXXXXXXXXXXXXX CONTROL XXXXXXXXXXXXXXXXXXXXXXXXXXX
+    // Funciones del menu
+    private fun menu(view: View) {
+        navController = Navigation.findNavController(view)
+        //Calificar app
+        binding.icMenu.btnStarBorder.setOnClickListener {
+            val nequiIcon = view.findViewById<ImageView>(R.id.btn_star_border)
+            nequiIcon.setOnClickListener {
+                // URL del enlace
+                val url = "https://play.google.com/store/apps/details?id=com.nequi.MobileApp&hl=es_419&gl=es"
+                // Intent para abrir la URL en un navegador o en la app de Google Play Store
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(url)
+                }
+                startActivity(intent) // Abre el enlace
+            }
+        }
+        //Como se juega
+        binding.icMenu.btnGame.setOnClickListener {
+            audioBackground.pause()
+            findNavController().navigate(R.id.action_game_to_instructions)
+            statusShowDialog(false)
+        }
+        //Audio ON/OFF
+        binding.icMenu.btnVolumeOn.setOnClickListener {
+            if (isMute) {
+                // Pausar la reproducción y actualizar el ícono a "apagado"
+                audioBackground.pause()
+                binding.icMenu.btnVolumeOn.setImageResource(R.drawable.ic_volume_off_30)
+                isMute= false
+            } else {
+                // Reanudar la reproducción y actualizar el ícono a "encendido"
+                audioBackground.start()
+                binding.icMenu.btnVolumeOn.setImageResource(R.drawable.ic_volume_on_30)
+                isMute= true
+            }
+        }
+        //Consultar y mostrar reto
+        binding.icMenu.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_game_to_challenges)
+        }
+        //Compartir app
+        binding.icMenu.btnShare.setOnClickListener {
+            val shareButton = view.findViewById<ImageView>(R.id.btn_share)
+            shareButton.setOnClickListener {
+                // Enlace de la aplicación en Google Play Store
+                val appPackageName = "com.nequi.MobileApp&hl=es_419&gl=es&pli=1" // Reemplaza con tu propio package name
+                val playStoreLink = "https://play.google.com/store/apps/details?id=$appPackageName"
+                val shareMessage = "App Bottle Flip\nSolo los valientes lo juegan!!\n$playStoreLink"
+                // Configura el Intent de compartir
+                val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, shareMessage)
+                }
+                startActivity(Intent.createChooser(shareIntent, "Compartir aplicación"))
+            }
         }
 
-        return view
-    }
-
-    private fun playBackgroundSound() {
-        if (backgroundMediaPlayer == null) {
-            backgroundMediaPlayer = MediaPlayer.create(
-                context,
-                R.raw.background_audio
-            ) // Cambia esto al nombre del archivo de sonido de fondo
-            backgroundMediaPlayer?.isLooping = true // Hacer que el sonido de fondo se repita
-            backgroundMediaPlayer?.start() // Reproduce el sonido de fondo
+        //Girar botella
+        binding.btnSpin.setOnClickListener {
+            spinBottle()
         }
     }
 
-    private fun stopBackgroundSound() {
-        backgroundMediaPlayer?.stop() // Detiene el sonido de fondo
-        backgroundMediaPlayer?.release() // Libera recursos
-        backgroundMediaPlayer = null // Resetea el objeto mediaPlayer
+    // Funciones de interacccion con el audio
+    private fun media() {
+        audioBackground = MediaPlayer.create(context, R.raw.background_audio)
+        audioSpinBottle = MediaPlayer.create(context, R.raw.audio_bottle)
+        //audioShowChallenge = MediaPlayer.create(context, R.raw.audio_challenge)
+        //audioButton = MediaPlayer.create(context, R.raw.audio_button)
+        //audioSuspense = MediaPlayer.create(context, R.raw.audio_suspense)
+        audioBackground.start()
     }
 
-    private fun playEffectSound() {
-        if (effectMediaPlayer == null) {
-            effectMediaPlayer = MediaPlayer.create(
-                context,
-                R.raw.audio_bottle
-            ) // Cambia esto al nombre del archivo del sonido de efecto
-            effectMediaPlayer?.start() // Reproduce el sonido de efecto
-            effectMediaPlayer?.setOnCompletionListener {
-                it.release() // Libera recursos al terminar
-                effectMediaPlayer = null // Resetea el objeto mediaPlayer
+    private fun bottle() {
+        bottleRotation()        //Movimiento botella
+        bottleButton()          //Boton botella
+        bottleChallenge()       //Mostrar reto
+    }
+
+    //XXXXXXXXXXXXXXXXXXXXXXX FUNCIONES BOTELLA XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    private fun bottleChallenge() {
+        statusShowDialog.observe(viewLifecycleOwner) {
+            if (it) {//Implementar consultar reto
+                //runBlocking {//Implementar contador
+                  //  delay(3 * 1000L)
+                //}
+                //showCountdown()
+                //audioSuspense.pause()
+               // val messageChallenge = "Debes tomar un trago"
+              //  dialogShowChallenge(
+               //     requireContext(),
+               //     audioBackground, isMute,
+              //      messageChallenge
+               // )
+                //audioShowChallenge.start()
+                audioSpinBottle.pause()
+                showCountdown()
+                //audioButton.pause()
+            }
+       }
+    }
+
+    private fun bottleButton() {
+        enableButton.observe(viewLifecycleOwner) { enableButton ->
+            binding.btnSpin.isVisible = enableButton
+        }
+    }
+
+    private fun bottleRotation() {
+        statusRotationBottle.observe(viewLifecycleOwner) { statusRotation ->
+            if (statusRotation) {
+                //audioButton.start()
+                audioBackground.pause()
+                audioSpinBottle.start()
+                rotationBottle.observe(viewLifecycleOwner) { rotation ->
+                    binding.centerImageView.startAnimation(rotation)
+                }
+            }
+        }
+    }
+
+    // Funciones
+    private fun spinBottle() {  //Girar botella
+        //countdownText.visibility = View.GONE    //Ocultar contador al girar la botella
+        _statusRotationBottle.value = true
+
+        val degrees = (Math.random() * 3600) + 1080
+        val rotation = RotateAnimation(
+            0f, degrees.toFloat(),
+            Animation.RELATIVE_TO_SELF,
+            0.5f, Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        rotation.fillAfter = true
+        rotation.duration = 3600
+        rotation.interpolator = DecelerateInterpolator()
+
+        rotation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                //_enabledStreamers.value = true
+                _enableButton.value = false
             }
 
-            // Detener el efecto de sonido después de 5 segundos
-            object : CountDownTimer(5000, 5000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    // No hacemos nada aquí
-                }
-
-                override fun onFinish() {
-                    stopEffectSound() // Detiene el sonido de efecto después de 5 segundos
-                }
-            }.start() // Iniciar el temporizador para el sonido de efecto
-        }
-    }
-
-    private fun stopEffectSound() {
-        effectMediaPlayer?.stop() // Detiene la reproducción del sonido de efecto
-        effectMediaPlayer?.release() // Libera los recursos
-        effectMediaPlayer = null // Resetea el objeto mediaPlayer
-    }
-
-
-    private fun rotateBottleClockwise() {
-
-        // Oculta el contador al iniciar la rotación
-        countdownText.visibility = View.GONE
-
-
-        // Genera un ángulo de rotación aleatorio entre 0 y 360 grados
-        val rotationAnimator = ValueAnimator.ofFloat(0f, 360f)
-        rotationAnimator.duration = 5000// 2 segundos de duración
-        rotationAnimator.addUpdateListener { animation ->
-            val animatedValue = animation.animatedValue as Float
-            bottleImage.rotation = animatedValue % 360// Actualiza la rotación de la botella
-        }
-        // Detener la rotación después de 1.5 segundos
-        rotationAnimator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animation: Animator) {
-                stopBottleSound() // Detiene el sonido al finalizar
-                showCountdown() // Muestra el contador después de que la botella se detiene
+            override fun onAnimationEnd(p0: Animation?) {
+                _statusRotationBottle.value = false
+                _enableButton.value = true
+               // _enabledStreamers.value = false
+                _statusShowDialog.value = true
+            }
+            override fun onAnimationRepeat(p0: Animation?) {
             }
         })
-        rotationAnimator.start()
+        _rotationBotle.value = rotation
     }
 
-    private fun showCountdown() {
-        countdownText.visibility = View.VISIBLE // Hacer visible el contador
-        countdownText.text = "3" // Comenzar con 3
+    private fun showCountdown() {//Mostrar y ocultar contador
+        binding.countdownText.visibility = View.VISIBLE // Hacer visible el contador
+        binding.countdownText.text = "3" // Comenzar con 3
 
-        object : CountDownTimer(4000, 1000) {
+         object : CountDownTimer(4000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 val secondsRemaining = (millisUntilFinished / 1000).toInt()
-                countdownText.text =
+                binding.countdownText.text =
                     secondsRemaining.toString() // Actualiza el texto con el tiempo restante
             }
 
             override fun onFinish() {
-                countdownText.text = "0" // Muestra 0 al final
+                binding.countdownText.text = "0" // Muestra 0 al final
+                binding.countdownText.visibility = View.GONE
             }
         }.start() // Inicia el temporizador
     }
 
-    private fun playBottleSound() {
-        if (effectMediaPlayer == null) {
-            effectMediaPlayer = MediaPlayer.create(context, R.raw.audio_bottle) // Carga el sonido
-            effectMediaPlayer?.start() // Reproduce el sonido
-            effectMediaPlayer?.setOnCompletionListener {
-                it.release() // Libera recursos al terminar
-                effectMediaPlayer = null // Resetea el objeto mediaPlayer
-            }
+    fun statusShowDialog(status: Boolean) {
+        _statusShowDialog.value = status
+    }
+
+    fun dialogShowChallenge(
+        context: Context,
+        audioBackground: MediaPlayer,
+        isMute: Boolean,
+        messageChallenge: String,
+    ) {
+       // showDialogChallenge(context, audioBackground, isMute, messageChallenge)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        audioBackground.stop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        audioBackground.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!isMute) {
+            audioBackground.start()
         }
-    }
-
-    private fun stopBottleSound() {
-        effectMediaPlayer?.stop() // Detiene la reproducción
-        effectMediaPlayer?.release() // Libera los recursos
-        effectMediaPlayer = null // Resetea el objeto mediaPlayer
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // Liberar el MediaPlayer cuando el fragmento se destruye
-        effectMediaPlayer?.release()
-        effectMediaPlayer = null
     }
 
 }
